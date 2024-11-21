9d26e642389ae9af2c04e45858e11f96875d206e
Piotr Zawadzki <115027061+squw0@users.noreply.github.com>
2024-11-21 11:09:24 +0100

    added rooms and doors

--- Labirynt/App.java
+++ Labirynt/App.java
@@ -13,7 +13,7 @@ public class App extends JFrame {
     private Image image;
 
     public App() {
-        setSize(200, 300);
+        setSize(300, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel = new MyJPanel();
         JButton button = new JButton("Draw");
@@ -44,8 +44,8 @@ public class App extends JFrame {
         Wall wall1 = new Wall(50, 100, Directions.NORTH);
         room1.setSite(Directions.NORTH, wall1);
     
-        Wall wall2 = new Wall(50, 100, Directions.SOUTH);
-        room1.setSite(Directions.SOUTH, wall2);
+        // Wall wall2 = new Wall(50, 100, Directions.SOUTH);
+        // room1.setSite(Directions.SOUTH, wall2);
     
         Wall wall3 = new Wall(50, 100, Directions.WEST);
         room1.setSite(Directions.WEST, wall3);
@@ -61,8 +61,8 @@ public class App extends JFrame {
         Wall wall21 = new Wall(50, 100, Directions.NORTH);
         room2.setSite(Directions.NORTH, wall21);
     
-        Wall wall22 = new Wall(50, 100, Directions.SOUTH);
-        room2.setSite(Directions.SOUTH, wall22);
+        // Wall wall22 = new Wall(50, 100, Directions.SOUTH);
+        // room2.setSite(Directions.SOUTH, wall22);
 
         // zamiast sciany są drzwi 
         // Wall wall23 = new Wall(50, 100, Directions.WEST);
@@ -71,14 +71,158 @@ public class App extends JFrame {
         Wall wall24 = new Wall(50, 100, Directions.EAST);
         room2.setSite(Directions.EAST, wall24);
     
+
+        // pokój nr 3
+        y += MapSite.lenght;
+        x -= MapSite.lenght;
+        Room room3 = new Room(x, y, nr++);
+        // Wall wall31 = new Wall(50, 100, Directions.NORTH);
+        // room3.setSite(Directions.NORTH, wall31);
+    
+        Wall wall32 = new Wall(50, 100, Directions.SOUTH);
+        room3.setSite(Directions.SOUTH, wall32);
+    
+        Wall wall33 = new Wall(50, 100, Directions.WEST);
+        room3.setSite(Directions.WEST, wall33);
+
+        // // zamiast sciany są drzwi 
+        // Wall wall34 = new Wall(50, 100, Directions.EAST);
+        // room3.setSite(Directions.EAST, wall34);
+
+    
+        // pokój nr 4
+        x += MapSite.lenght;
+        Room room4 = new Room(x, y, nr++);
+        // Wall wall41 = new Wall(50, 100, Directions.NORTH);
+        // room4.setSite(Directions.NORTH, wall41);
+    
+        Wall wall42 = new Wall(50, 100, Directions.SOUTH);
+        room4.setSite(Directions.SOUTH, wall42);
+    
+        // Wall wall43 = new Wall(50, 100, Directions.WEST);
+        // room4.setSite(Directions.WEST, wall43);
+
+        Wall wall44 = new Wall(50, 100, Directions.EAST);
+        room4.setSite(Directions.EAST, wall44);
+
+
+        // pokój nr 5
+        x += MapSite.lenght;
+        Room room5 = new Room(x, y, nr++);
+        Wall wall51 = new Wall(50, 100, Directions.NORTH);
+        room5.setSite(Directions.NORTH, wall51);
+    
+        Wall wall52 = new Wall(50, 100, Directions.SOUTH);
+        room5.setSite(Directions.SOUTH, wall52);
+    
+        Wall wall53 = new Wall(50, 100, Directions.WEST);
+        room5.setSite(Directions.WEST, wall53);
+
+        Wall wall54 = new Wall(50, 100, Directions.EAST);
+        room5.setSite(Directions.EAST, wall54);
+
+
+        // pokój nr 6
+        y -= MapSite.lenght;
+        Room room6 = new Room(x, y, nr++);
+        Wall wall61 = new Wall(50, 100, Directions.NORTH);
+        room6.setSite(Directions.NORTH, wall61);
+    
+        Wall wall62 = new Wall(50, 100, Directions.SOUTH);
+        room6.setSite(Directions.SOUTH, wall62);
+    
+        Wall wall63 = new Wall(50, 100, Directions.WEST);
+        room6.setSite(Directions.WEST, wall63);
+
+        Wall wall64 = new Wall(50, 100, Directions.EAST);
+        room6.setSite(Directions.EAST, wall64);
+
+
+        // pokój nr 7
+        y -= MapSite.lenght;
+        Room room7 = new Room(x, y, nr++);
+        Wall wall71 = new Wall(50, 100, Directions.NORTH);
+        room7.setSite(Directions.NORTH, wall71);
+    
+        Wall wall72 = new Wall(50, 100, Directions.SOUTH);
+        room7.setSite(Directions.SOUTH, wall72);
+    
+        Wall wall73 = new Wall(50, 100, Directions.WEST);
+        room7.setSite(Directions.WEST, wall73);
+
+        Wall wall74 = new Wall(50, 100, Directions.EAST);
+        room7.setSite(Directions.EAST, wall74);
+
+
+        // pokój nr 8
+        x -= MapSite.lenght;
+        Room room8 = new Room(x, y, nr++);
+        Wall wall81 = new Wall(50, 100, Directions.NORTH);
+        room8.setSite(Directions.NORTH, wall81);
+    
+        Wall wall82 = new Wall(50, 100, Directions.SOUTH);
+        room8.setSite(Directions.SOUTH, wall82);
+    
+        Wall wall83 = new Wall(50, 100, Directions.WEST);
+        room8.setSite(Directions.WEST, wall83);
+
+        Wall wall84 = new Wall(50, 100, Directions.EAST);
+        room8.setSite(Directions.EAST, wall84);
+
+
+        // pokój nr 9
+        x -= MapSite.lenght;
+        Room room9 = new Room(x, y, nr++);
+        Wall wall91 = new Wall(50, 100, Directions.NORTH);
+        room9.setSite(Directions.NORTH, wall91);
+    
+        Wall wall92 = new Wall(50, 100, Directions.SOUTH);
+        room9.setSite(Directions.SOUTH, wall92);
+    
+        Wall wall93 = new Wall(50, 100, Directions.WEST);
+        room9.setSite(Directions.WEST, wall93);
+
+        Wall wall94 = new Wall(50, 100, Directions.EAST);
+        room9.setSite(Directions.EAST, wall94);
+
+    
+        // drzwi pion 1 - 2 
         Door door = new Door(room1, room2);
         room1.setSite(Directions.EAST, door);  
-        room2.setSite(Directions.WEST, door);  
-    
+        room2.setSite(Directions.WEST, door); 
+
+
+        // drzwi pion 3 - 4 
+        Door door1 = new Door(room3, room4);
+        room3.setSite(Directions.EAST, door1);  
+        room4.setSite(Directions.WEST, door1);
+
+
+        // drzwi poziom 1 - 3 
+        Door door3 = new Door(room1, room3);
+        room1.setSite(Directions.SOUTH, door3);  
+        room3.setSite(Directions.NORTH, door3);
+
+
+        // drzwi poziom 2 - 4
+        Door door4 = new Door(room2, room4);
+        room2.setSite(Directions.SOUTH, door4);  
+        room4.setSite(Directions.NORTH, door4);  
+
+
+        // rysowanie pokoi
         room1.draw(image);
         room2.draw(image);
+        room3.draw(image);
+        room4.draw(image);
+        room5.draw(image);
+        room6.draw(image);
+        room7.draw(image);
+        room8.draw(image);
+        room9.draw(image);
     
-        panel.repaint(); // Odswieżenie panela po ustawieniu pokoju
+        // Odswieżenie panela po ustawieniu pokoju
+        panel.repaint(); 
     }
     
 
--- Labirynt/Door.java
+++ Labirynt/Door.java
@@ -13,6 +13,8 @@ public class Door extends MapSite {
         this.roomTwo = room2;
         this.isOpen = isOpen;
     }
+
+    
     // jakby sie cos zepsuło
     //     if (x1==x2) { // pokoje leza w pionie, sciana z drzwiami jest pozioma
     //         if(y1 > y2)
@@ -36,23 +38,43 @@ public class Door extends MapSite {
         Graphics g = image.getGraphics();
         int doorLength = MapSite.lenght / 3;
     
+        // if (x1 == x2) {
+        //     // pokoje leza w pionie, sciana z drzwiami jest pozioma
+        //     int y = Math.min(y1, y2);  
+        //     int startX = x1; // poczatek sciany
+        //     int endX = startX + MapSite.lenght; // koniec sciany
+    
+        //     g.drawLine(startX, y, startX + (MapSite.lenght - doorLength) / 2, y); // początek sciany poziom - drzwi
+        //     g.drawLine(endX - (MapSite.lenght - doorLength) / 2, y, endX, y); // drzwi - koniec sciany poziom
+    
+        // } else {  
+        //     // pokoje leza w poziomie scaina z drzwiami jest pionowa
+        //     int x = Math.min(x1, x2) + MapSite.lenght;  // łączenie albo stykanie pokoi 
+        //     int startY = y1;
+        //     int endY = startY + MapSite.lenght;
+    
+        //     g.drawLine(x, startY, x, startY + (MapSite.lenght - doorLength) / 2); // początek sciany pion - drzwi
+        //     g.drawLine(x, endY - (MapSite.lenght - doorLength) / 2, x, endY); //  drzwi - koniec sciany pion
+        // }
+
         if (x1 == x2) {
-            // pokoje leza w pionie, sciana z drzwiami jest pozioma
-            int y = Math.min(y1, y2);  
-            int startX = x1; // poczatek sciany
-            int endX = startX + MapSite.lenght; // koniec sciany
-    
-            g.drawLine(startX, y, startX + (MapSite.lenght - doorLength) / 2, y); // początek sciany poziom - drzwi
-            g.drawLine(endX - (MapSite.lenght - doorLength) / 2, y, endX, y); // drzwi - koniec sciany poziom
-    
+            // Pokoje leżą w pionie (pozioma ściana z drzwiami)
+            int y = Math.max(y1, y2);  // y to wspólna współrzędna ściany (dolna krawędź górnego pokoju)
+            int startX = x1;           // Początek ściany poziomej
+            int endX = startX + MapSite.lenght; // Koniec ściany poziomej
+        
+            // Rysujemy dwie części ściany, pomijając drzwi na środku
+            g.drawLine(startX, y, startX + (MapSite.lenght - doorLength) / 2, y); // Lewa część ściany
+            g.drawLine(endX - (MapSite.lenght - doorLength) / 2, y, endX, y);     // Prawa część ściany
         } else {  
-            // pokoje leza w poziomie scaina z drzwiami jest pionowa
-            int x = Math.min(x1, x2) + MapSite.lenght;  // łączenie albo stykanie pokoi 
-            int startY = y1;
-            int endY = startY + MapSite.lenght;
-    
-            g.drawLine(x, startY, x, startY + (MapSite.lenght - doorLength) / 2); // początek sciany pion - drzwi
-            g.drawLine(x, endY - (MapSite.lenght - doorLength) / 2, x, endY); //  drzwi - koniec sciany pion
+            // Pokoje leżą w poziomie (pionowa ściana z drzwiami)
+            int x = Math.max(x1, x2);  // x to wspólna współrzędna ściany (prawa krawędź lewego pokoju)
+            int startY = y1;           // Początek ściany pionowej
+            int endY = startY + MapSite.lenght; // Koniec ściany pionowej
+        
+            // Rysujemy dwie części ściany, pomijając drzwi na środku
+            g.drawLine(x, startY, x, startY + (MapSite.lenght - doorLength) / 2); // Górna część ściany
+            g.drawLine(x, endY - (MapSite.lenght - doorLength) / 2, x, endY);     // Dolna część ściany
         }
     }
 
